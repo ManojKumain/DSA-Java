919987591a62ae6fe0fe545a03994c32cff67a8e
Manoj Chand <memanojkumain@gmail.com>
2026-03-06 11:04:52 +0530

    method call and local variables in Java with execution

---
+++ PHASE 0/2-method-call-and-local-variables/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static int multiplyNumbers (int a, int b) {
+        int multiply = a * b;
+        return multiply;
+    }
+    public static void main(String[] args) {
+        int  result = multiplyNumbers(3, 5);
+        System.out.println(result);
+    }
+
+
+}
