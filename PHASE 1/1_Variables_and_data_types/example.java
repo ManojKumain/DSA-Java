b51786c03f91df58b286ede8e04dff734132086e
Manoj Chand <memanojkumain@gmail.com>
2026-03-07 09:21:55 +0530

    PHASE 1: Variables and mini challenges completed

---
+++ PHASE 1/1_Variables_and_data_types/example.java
@@ -0,0 +1,10 @@
+public class example {
+
+    public static void main(String[] args) {
+        String name = "Manoj";
+        int age = 26;
+        String city = "Dehradun";
+        String college = "GRD IMT";
+        System.out.println("My name is " + name + ". My age is " + age + " and I live in " + city + ". I study at " + college + ".");
+    }
+}
